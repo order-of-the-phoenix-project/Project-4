d21a59519f21a422f3d37081a0dc7fd9a22b28ce
SirWatson <danielwatson11@gmail.com>
2019-02-28 13:49:41 -0600

    Created initial server file

---
+++ server.py
@@ -0,0 +1,44 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+# def factorial(num):
+#     total = num
+#     for i in range(1, num - 1):
+#         num -= 1
+#         total = total * (num)
+#     return total 
+        
+        
+        
+@app.route('/')
+def index():
+    return "it works"
+
+# @app.route('/md5/<string>')
+# def handle_md5(string):
+#     h = hashlib.md5(bytes(string, 'utf-8')).hexdigest()
+#     return h
+
+@app.route('/factorial/<num>')
+def handle_factorial(num):
+
+    use = int(num)
+    total = int(num)
+    for i in range(1, int(num) - 1):
+        use -= 1
+        total = total * use
+    return str(total)
+
+@app.route('/is-prime/<int>')
+def handle_prime(int):
+
+    
+    return
+
+# @app.route('/slack-alert/<string>')
+
+app.debug = True
+
+
+app.run()
