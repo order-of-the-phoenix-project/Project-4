4846973f1e2ffb2c03803c1d9ac0c19c415f9e97
Jorgelebrija <Jorgelebrija@users.noreply.github.com>
2019-03-06 19:13:52 -0600

    Commit1

--- server.py
+++ server.py
@@ -60,6 +60,21 @@ def handle_prime(int):
 # @app.route('/is-prime/<int>')
 # def handle_prime(int):
 
+# prime numbers are greater than 1
+    if num > 1:
+   # check for factors
+        for i in range(2,num):
+            if (num % i) == 0:
+                print(num,"is not a prime number")
+                print(num, "is a prime number")
+                break
+        else:
+            print(num,"is a prime number")
+       
+# if input number is less than
+# or equal to 1, it is not prime
+    else:
+        print(num,"is not a prime number")
     
 #     return
 
